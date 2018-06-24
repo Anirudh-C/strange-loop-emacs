4a76834ccb6009c7e3b8dfced9230c9b64255710
Anirudh <anirudhvan@gmail.com>
2018-06-24 20:54:50 +0530

    Added hydra

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (js2-mode markdown-mode helm evil-easymotion evil spaceline-config delight zenburn-theme all-the-icons solarized-theme spaceline multiple-cursors smart-mode-line-powerline-theme web-mode which-key emmet-mode evil-escape helm-projectile projectile smartparens evil-surround evil-nerd-commenter evil-nerd-commentor evil-magit magit restart-emacs evil-leader use-package evil-visual-mark-mode))))
+    (hydra js2-mode markdown-mode helm evil-easymotion evil spaceline-config delight zenburn-theme all-the-icons solarized-theme spaceline multiple-cursors smart-mode-line-powerline-theme web-mode which-key emmet-mode evil-escape helm-projectile projectile smartparens evil-surround evil-nerd-commenter evil-nerd-commentor evil-magit magit restart-emacs evil-leader use-package evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
