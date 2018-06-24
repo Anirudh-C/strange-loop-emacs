128f51f93f1a850cfb986ea228170170d0facb50
Anirudh <anirudhvan@gmail.com>
2018-06-25 00:34:24 +0530

    added org mode config

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (hydra js2-mode markdown-mode helm evil-easymotion evil spaceline-config delight zenburn-theme all-the-icons solarized-theme spaceline multiple-cursors smart-mode-line-powerline-theme web-mode which-key emmet-mode evil-escape helm-projectile projectile smartparens evil-surround evil-nerd-commenter evil-nerd-commentor evil-magit magit restart-emacs evil-leader use-package evil-visual-mark-mode))))
+    (evil-mu4e mu4e hydra js2-mode markdown-mode helm evil-easymotion evil spaceline-config delight zenburn-theme all-the-icons solarized-theme spaceline multiple-cursors smart-mode-line-powerline-theme web-mode which-key emmet-mode evil-escape helm-projectile projectile smartparens evil-surround evil-nerd-commenter evil-nerd-commentor evil-magit magit restart-emacs evil-leader use-package evil-visual-mark-mode)))
+ '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
