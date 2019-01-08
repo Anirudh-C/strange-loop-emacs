7a502f4732e8a4471b7a49f138f851b7128ef073
Anirudh <anirudhvan@gmail.com>
2019-01-08 19:22:28 +0530

    Major rehaul of config: Organised structure and made major changes

--- custom.el
+++ custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ivy-mode t)
+ '(ivy-virtual-abbreviate (quote full))
  '(package-selected-packages
    (quote
-    (projectile-rails evil-org org-bullets try exwm nov speed-type impatient-mode htmlize simple-httpd pdf-tools ranger magithub cider clojure-mode ascii-art-to-unicode org-brain org-plus-contrib rainbow-mode company eldoc-eval shrink-path doom-modeline powerline evil-mu4e mu4e hydra js2-mode markdown-mode helm evil-easymotion evil spaceline-config delight zenburn-theme all-the-icons solarized-theme spaceline multiple-cursors smart-mode-line-powerline-theme web-mode which-key emmet-mode evil-escape helm-projectile projectile smartparens evil-surround evil-nerd-commenter evil-nerd-commentor evil-magit magit restart-emacs evil-leader use-package evil-visual-mark-mode)))
+    (pocket-reader elpy elm-mode rjsx-mode json-mode indium company-web ace-window browse-kill-ring easy-kill xkcd flx ivy-rich counsel swiper ivy projectile-rails evil-org org-bullets try exwm nov speed-type impatient-mode htmlize simple-httpd pdf-tools ranger magithub cider clojure-mode ascii-art-to-unicode org-brain org-plus-contrib rainbow-mode company eldoc-eval shrink-path doom-modeline powerline evil-mu4e mu4e hydra js2-mode markdown-mode helm evil-easymotion evil spaceline-config delight zenburn-theme all-the-icons solarized-theme spaceline multiple-cursors smart-mode-line-powerline-theme web-mode which-key emmet-mode evil-escape helm-projectile projectile smartparens evil-surround evil-nerd-commenter evil-nerd-commentor evil-magit magit restart-emacs evil-leader use-package evil-visual-mark-mode)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
