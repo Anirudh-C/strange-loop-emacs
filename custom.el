76fceec7b515c93ed43eb20b01bc08b4c75052e7
Anirudh C <anirudh.c@iiitb.org>
2020-07-26 14:17:33 +0530

    fix elpy and add bibtex and org-publish for RE

--- custom.el
+++ custom.el
@@ -3,11 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(deft-default-extension "org" t)
+ '(deft-directory "~/org/brain" t)
+ '(deft-recursive t t)
+ '(deft-use-filter-string-for-filename t t)
  '(ivy-mode t)
  '(ivy-virtual-abbreviate (quote full))
+ '(org-roam-directory "~/org/brain")
  '(package-selected-packages
    (quote
-    (deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package)))
+    (org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
