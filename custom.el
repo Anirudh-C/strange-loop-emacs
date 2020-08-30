a93e7627649c5d098a9a4b7a837d96ad3ec87e52
Anirudh C <anirudh.c@iiitb.org>
2020-08-30 12:57:10 +0530

    add: which key with posframe and update RE folder

--- custom.el
+++ custom.el
@@ -12,5 +12,12 @@
  '(org-roam-directory "~/org/brain")
  '(package-selected-packages
    (quote
-    (org-pomodoro org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package)))
- '(send-mail-function (quote mailclient-send-it)))
+    (which-key-posframe which-key org-pomodoro org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package)))
+ '(send-mail-function (quote mailclient-send-it))
+ '(which-key-posframe-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(trailing-whitespace ((t (:background "#6272a4")))))
