3c8761b3b5e4c0dc01d539163bf32b3f2e8c81b5
Anirudh C <anirudh.c@iiitb.org>
2022-08-25 19:01:44 +0530

    update: clean up with nano

--- custom.el
+++ custom.el
@@ -3,18 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(deft-default-extension "org" t)
- '(deft-directory "~/org/brain" t)
- '(deft-recursive t t)
- '(deft-use-filter-string-for-filename t t)
  '(ivy-mode t)
- '(ivy-virtual-abbreviate (quote full))
- '(org-agenda-files (quote ("~/.emacs.d/config.org" "~/org/index.org")))
+ '(ivy-virtual-abbreviate 'full)
+ '(org-agenda-files '("~/.emacs.d/config.org" "~/org/index.org"))
  '(org-roam-directory "~/org/brain")
  '(package-selected-packages
-   (quote
-    (epc auctex org-ref helm-projectile swiper-helm helm-flx helm simple-modeline doom-modeline org-roam-server engine-mode which-key-posframe which-key org-pomodoro org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package)))
- '(send-mail-function (quote mailclient-send-it))
+   '(orgalist org mini-frame nano-emacs lambda-line lambda-themes quelpa-use-package quelpa org-contrib elfeed-score elfeed solo-jazz-theme epc auctex org-ref helm-projectile swiper-helm helm-flx helm simple-modeline doom-modeline org-roam-server engine-mode which-key-posframe which-key org-pomodoro org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package))
+ '(send-mail-function 'mailclient-send-it)
  '(which-key-posframe-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
