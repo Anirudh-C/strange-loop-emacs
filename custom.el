1aac561a7f984283da5cb890e89b8cef1e57ea57
Anirudh Chandramouli <anirudhvan@gmail.com>
2026-04-20 18:27:25 +0300

    add: planning and clean up system

--- custom.el
+++ custom.el
@@ -7,7 +7,13 @@
  '(ivy-virtual-abbreviate 'full)
  '(org-agenda-files '("/home/strangeloop/org/index.org"))
  '(package-selected-packages
-   '(modus-themes org-modern hc-zenburn-theme zenburn-theme autothemer atomic-chrome company helm-org-ql org-inline-pdf bibliography helm-bibtex org-roam-ui websocket orgalist org mini-frame nano-emacs lambda-line lambda-themes quelpa-use-package quelpa org-contrib elfeed-score elfeed solo-jazz-theme epc auctex org-ref helm-projectile swiper-helm helm-flx helm simple-modeline doom-modeline org-roam-server engine-mode which-key-posframe which-key org-pomodoro org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package))
+   '(org-brain-walk modus-themes org-modern hc-zenburn-theme zenburn-theme autothemer atomic-chrome company helm-org-ql org-inline-pdf bibliography helm-bibtex org-roam-ui websocket orgalist org mini-frame nano-emacs lambda-line lambda-themes quelpa-use-package quelpa org-contrib elfeed-score elfeed solo-jazz-theme epc auctex org-ref helm-projectile swiper-helm helm-flx helm simple-modeline doom-modeline org-roam-server engine-mode which-key-posframe which-key org-pomodoro org-plus-contrib elpy deft el-patch org-noter org-roam org-bullets nov pdf-tools cider clojure-mode elm-mode rjsx-mode json-mode impatient-mode htmlize simple-httpd company-web web-mode emmet-mode rainbow-mode markdown-mode god-mode ace-window browse-kill-ring easy-kill multiple-cursors neotree ivy-rich counsel flx ivy projectile magit undo-tree restart-emacs try telephone-line dracula-theme rainbow-delimiters use-package))
  '(pdf-view-midnight-colors '("#dcdccc" . "#1c1c1c"))
  '(send-mail-function 'mailclient-send-it)
  '(which-key-posframe-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
